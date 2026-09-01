c1cbc6b669ddc31a66c91791da8b48fdf50e10b8
Johan Castillo <johancal30@gmail.com>
2026-09-01 07:29:54 -0500

    Modulo de registro de usuarios

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login 
 
+def registro(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirige a la página de inicio después del registro
+    else:
+         form = UserCreationForm()
+    return render(request, 'registro.html', {'form': form})
 
